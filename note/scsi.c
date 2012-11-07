538ca28b2ab923cdbc6978b3002dce777b04b5e5
babyaries <babyaries@126.com>
2012-11-07 11:53:11 +0800

    update scsi note

--- note/scsi.c
+++ note/scsi.c
@@ -1,3 +1,13 @@
+disk_map_sector_rcu ()
+hd_struct 			/* one partition */
+struct disk_part_tbl {
+  struct hd_struct __rcu *lask_lookup; /* one partition */
+  struct hd_struct __rcu *part[];
+};
+struct gendisk {
+  struct disk_part_tbl __rcu *part_tbl; /* partition table */
+};
+
 kernel_thread_helper -> kthread_worker_fn -> kthread -> \
 __do_softirq -> run_ksoftirqd -> __do_softirq -> blk_done_softirq -> \
 scsi_softirq_done -> scsi_finish_command -> sici_io_sompletion -> scsi_next_command -> \
